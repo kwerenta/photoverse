3f9a6614482a20f668fa401192bf317408d79af4
Kamil Wenta <kamil.wenta222@gmail.com>
2024-07-02 23:52:10 +0200

    Fix inverse association for User and Follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable
 
-  has_many :received_follows, foreign_key: :following_id, class_name: "Follow", dependent: :destroy, inverse_of: :user
+  has_many :received_follows, foreign_key: :following_id, class_name: "Follow", dependent: :destroy,
+    inverse_of: :following
   has_many :followers, through: :received_follows, source: :follower
-  has_many :given_follows, foreign_key: :follower_id, class_name: "Follow", dependent: :destroy, inverse_of: :user
+
+  has_many :given_follows, foreign_key: :follower_id, class_name: "Follow", dependent: :destroy, inverse_of: :follower
   has_many :followings, through: :given_follows, source: :following
 
   has_many :likes, dependent: :destroy
