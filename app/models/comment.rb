c177417033e09f79c15bb230eebc9d63bdc34c90
Kamil Wenta <kamil.wenta222@gmail.com>
2025-09-16 09:52:34 +0200

    Add likes to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post, counter_cache: :comments_count
   belongs_to :parent, class_name: "Comment", optional: true
+
   has_many :replies, class_name: "Comment", foreign_key: :parent_id, dependent: :destroy, inverse_of: :parent
+  has_many :likes, as: :likeable, dependent: :destroy
 
   validates :content, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_28_112700) do
+ActiveRecord::Schema[7.0].define(version: 2025_09_16_074532) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_28_112700) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "parent_id"
+    t.integer "likes_count", default: 0, null: false
     t.index ["parent_id"], name: "index_comments_on_parent_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
